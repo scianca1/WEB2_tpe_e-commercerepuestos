d0b868f5754671bb8b95de4f00e2e09752be3040
Matias Sciancalepore <msciancalepore47@gmail.com>
2022-10-05 23:18:55 -0300

    hice andar el css

--- templates_c/0bf8ed6608085e82c2dad86a104ff474411dbb8c_0.file.header.tpl.php
+++ templates_c/0bf8ed6608085e82c2dad86a104ff474411dbb8c_0.file.header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-10-06 03:07:40
+/* Smarty version 4.2.1, created on 2022-10-06 04:16:31
   from 'C:\xampp\htdocs\web2\repuestos\templates\header.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_633e2a5cf30f48_18546945',
+  'unifunc' => 'content_633e3a7fa1d5e3_68473144',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0bf8ed6608085e82c2dad86a104ff474411dbb8c' => 
     array (
       0 => 'C:\\xampp\\htdocs\\web2\\repuestos\\templates\\header.tpl',
-      1 => 1665018457,
+      1 => 1665022585,
       2 => 'file',
     ),
   ),
@@ -20,15 +20,17 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_633e2a5cf30f48_18546945 (Smarty_Internal_Template $_smarty_tpl) {
+function content_633e3a7fa1d5e3_68473144 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+    <base href="'.BASE_URL.'">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solo Hidraulica</title>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL;?>
+/style/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
--- templates_c/ecaed49b796dddb5cdbf83e687e9912db8a44308_0.file.unproducto.tpl.php
+++ templates_c/ecaed49b796dddb5cdbf83e687e9912db8a44308_0.file.unproducto.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-10-06 02:31:13
+/* Smarty version 4.2.1, created on 2022-10-06 04:12:22
   from 'C:\xampp\htdocs\web2\repuestos\templates\unproducto.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_633e21d1d45018_60154774',
+  'unifunc' => 'content_633e3986d454c4_13348497',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ecaed49b796dddb5cdbf83e687e9912db8a44308' => 
     array (
       0 => 'C:\\xampp\\htdocs\\web2\\repuestos\\templates\\unproducto.tpl',
-      1 => 1665015638,
+      1 => 1665018386,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:templates/footer.tpl' => 1,
   ),
 ),false)) {
-function content_633e21d1d45018_60154774 (Smarty_Internal_Template $_smarty_tpl) {
+function content_633e3986d454c4_13348497 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender('file:templates/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 <div class= "producto">
